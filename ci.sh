d7f21d8c1b5313d839ab0c9e362dc92ef89db954
Asa Oines <asaoines@gmail.com>
2023-04-03 17:00:42 -0400

    it works

--- ci.sh
+++ ci.sh
@@ -50,5 +50,6 @@ docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host
 
 DEBUG=hyperlane* yarn ts-node scripts/test.ts --chains anvil1 anvil2 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --timeout 60
 
+docker ps -aq | xargs docker stop | xargs docker rm
 kill $ANVIL_1_PID
 kill $ANVIL_2_PID
