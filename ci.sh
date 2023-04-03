f0c84a2ecc29007c515468f95e857a77a5a3baa1
Asa Oines <asaoines@gmail.com>
2023-04-03 17:21:04 -0400

    cleanup

--- ci.sh
+++ ci.sh
@@ -1,7 +1,9 @@
-mkdir ./artifacts/anvil1 ./artifacts/anvil1/state ./artifacts/anvil1/validator ./artifacts/anvil1/relayer
-chmod 777 ./artifacts/anvil1 -R
-mkdir ./artifacts/anvil2 ./artifacts/anvil2/state ./artifacts/anvil2/validator ./artifacts/anvil2/relayer
-chmod 777 ./artifacts/anvil2 -R
+for CHAIN in anvil1 anvil2
+do
+    mkdir ./artifacts/$CHAIN ./artifacts/$CHAIN/state \
+      ./artifacts/$CHAIN/validator ./artifacts/$CHAIN/relayer
+    chmod 777 ./artifacts/$CHAIN -R
+done
 
 anvil --chain-id 31337 -p 8545 --state ./artifacts/anvil1/state > /dev/null &
 ANVIL_1_PID=$!
@@ -13,38 +15,59 @@ sleep 1
 
 set -e
 
-echo "Deploying contracts to anvil1"
-DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil1 --remotes anvil2 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --no-write-agent-config
-echo "Deploying contracts to anvil2"
-DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil2 --remotes anvil1 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-
-# Won't work on anything but linux
-echo "Running validator on anvil1"
-docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host -e CONFIG_FILES=/config/agent_config.json -e HYP_VALIDATOR_ORIGINCHAINNAME=anvil1 -e HYP_VALIDATOR_REORGPERIOD=1 -e HYP_VALIDATOR_INTERVAL=1 -e HYP_BASE_CHAINS_ANVIL1_CONNECTION_URL=http://127.0.0.1:8545 -e HYP_VALIDATOR_VALIDATOR_TYPE=hexKey -e HYP_VALIDATOR_VALIDATOR_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 -e HYP_VALIDATOR_CHECKPOINTSYNCER_TYPE=localStorage -e HYP_VALIDATOR_CHECKPOINTSYNCER_PATH=/config/anvil1/validator -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./validator &
-echo "Running validator on anvil2"
-docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host -e CONFIG_FILES=/config/agent_config.json -e HYP_VALIDATOR_ORIGINCHAINNAME=anvil2 -e HYP_VALIDATOR_REORGPERIOD=1 -e HYP_VALIDATOR_INTERVAL=1 -e HYP_BASE_CHAINS_ANVIL2_CONNECTION_URL=http://127.0.0.1:8555 -e HYP_VALIDATOR_VALIDATOR_TYPE=hexKey -e HYP_VALIDATOR_VALIDATOR_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 -e HYP_VALIDATOR_CHECKPOINTSYNCER_TYPE=localStorage -e HYP_VALIDATOR_CHECKPOINTSYNCER_PATH=/config/anvil2/validator -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./validator &
-
-sleep 10
-
-echo "Announcing validator on anvil1"
-VALIDATOR_ANNOUNCE_ADDRESS=$(cat ./artifacts/addresses.json | jq -r '.anvil1.validatorAnnounce')
-VALIDATOR=$(cat ./artifacts/anvil1/validator/announcement.json | jq -r '.value.validator')
-STORAGE_LOCATION=$(cat ./artifacts/anvil1/validator/announcement.json | jq -r '.value.storage_location')
-SIGNATURE=$(cat ./artifacts/anvil1/validator/announcement.json | jq -r '.serialized_signature')
-cast send $VALIDATOR_ANNOUNCE_ADDRESS  "announce(address, string calldata, bytes calldata)(bool)" $VALIDATOR $STORAGE_LOCATION $SIGNATURE --rpc-url http://127.0.0.1:8545 --private-key 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+for i in "anvil1 anvil2 --no-write-agent-config" "anvil2 anvil1 --write-agent-config"
+do
+    set -- $i
+    echo "Deploying contracts to $1"
+    DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local $1 --remotes $2 \
+    --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 $3
+done
 
 
-echo "Announcing validator on anvil2"
-VALIDATOR_ANNOUNCE_ADDRESS=$(cat ./artifacts/addresses.json | jq -r '.anvil2.validatorAnnounce')
-VALIDATOR=$(cat ./artifacts/anvil2/validator/announcement.json | jq -r '.value.validator')
-STORAGE_LOCATION=$(cat ./artifacts/anvil2/validator/announcement.json | jq -r '.value.storage_location')
-SIGNATURE=$(cat ./artifacts/anvil2/validator/announcement.json | jq -r '.serialized_signature')
-cast send $VALIDATOR_ANNOUNCE_ADDRESS  "announce(address, string calldata, bytes calldata)(bool)" $VALIDATOR $STORAGE_LOCATION $SIGNATURE --rpc-url http://127.0.0.1:8555 --private-key 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+for i in "anvil1 8545 ANVIL1" "anvil2 8555 ANVIL2"
+do
+    set -- $i
+    echo "Running validator on $1"
+    # Won't work on anything but linux due to -net=host
+    docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host \
+      -e CONFIG_FILES=/config/agent_config.json -e HYP_VALIDATOR_ORIGINCHAINNAME=$1 \
+      -e HYP_VALIDATOR_REORGPERIOD=1 -e HYP_VALIDATOR_INTERVAL=1 \
+      -e HYP_BASE_CHAINS_${3}_CONNECTION_URL=http://127.0.0.1:${2} \
+      -e HYP_VALIDATOR_VALIDATOR_TYPE=hexKey \
+      -e HYP_VALIDATOR_VALIDATOR_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6 \
+      -e HYP_VALIDATOR_CHECKPOINTSYNCER_TYPE=localStorage \
+      -e HYP_VALIDATOR_CHECKPOINTSYNCER_PATH=/config/${1}/validator \
+      -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty \
+      gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./validator &
+    sleep 5
+    echo "Announcing validator on $1"
+    VALIDATOR_ANNOUNCE_ADDRESS=$(cat ./artifacts/addresses.json | jq -r ".$1.validatorAnnounce")
+    VALIDATOR=$(cat ./artifacts/$1/validator/announcement.json | jq -r '.value.validator')
+    STORAGE_LOCATION=$(cat ./artifacts/$1/validator/announcement.json | jq -r '.value.storage_location')
+    SIGNATURE=$(cat ./artifacts/$1/validator/announcement.json | jq -r '.serialized_signature')
+    cast send $VALIDATOR_ANNOUNCE_ADDRESS  \
+      "announce(address, string calldata, bytes calldata)(bool)" \
+      $VALIDATOR $STORAGE_LOCATION $SIGNATURE --rpc-url http://127.0.0.1:$2 \
+      --private-key 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
+done
 
-echo "Running relayer on anvil1"
-docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host -e CONFIG_FILES=/config/agent_config.json -e HYP_BASE_CHAINS_ANVIL1_CONNECTION_URL=http://127.0.0.1:8545 -e HYP_BASE_CHAINS_ANVIL2_CONNECTION_URL=http://127.0.0.1:8555 -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty -e HYP_RELAYER_ORIGINCHAINNAME=anvil1 -e HYP_RELAYER_DESTINATIONCHAINNAMES=anvil2 -e HYP_RELAYER_ALLOWLOCALCHECKPOINTSYNCERS=true -e HYP_RELAYER_DB=/config/anvil1/relayer -e HYP_RELAYER_GASPAYMENTENFORCEMENT='[{"type":"none"}]' -e HYP_BASE_CHAINS_ANVIL2_SIGNER_TYPE=hexKey -e HYP_BASE_CHAINS_ANVIL2_SIGNER_KEY=0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97 gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./relayer &
-echo "Running relayer on anvil2"
-docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host -e CONFIG_FILES=/config/agent_config.json -e HYP_BASE_CHAINS_ANVIL1_CONNECTION_URL=http://127.0.0.1:8545 -e HYP_BASE_CHAINS_ANVIL2_CONNECTION_URL=http://127.0.0.1:8555 -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty -e HYP_RELAYER_ORIGINCHAINNAME=anvil2 -e HYP_RELAYER_DESTINATIONCHAINNAMES=anvil1 -e HYP_RELAYER_ALLOWLOCALCHECKPOINTSYNCERS=true -e HYP_RELAYER_DB=/config/anvil2/relayer -e HYP_RELAYER_GASPAYMENTENFORCEMENT='[{"type":"none"}]' -e HYP_BASE_CHAINS_ANVIL1_SIGNER_TYPE=hexKey -e HYP_BASE_CHAINS_ANVIL1_SIGNER_KEY=0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97 gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./relayer &
+
+for i in "anvil1 ANVIL1 ANVIL2" "anvil2 ANVIL2 ANVIL1"
+do
+    set -- $i
+    echo "Running relayer on $1"
+    docker run --mount type=bind,source="$(pwd)/artifacts",target=/config --net=host \
+      -e CONFIG_FILES=/config/agent_config.json \
+      -e HYP_BASE_CHAINS_ANVIL1_CONNECTION_URL=http://127.0.0.1:8545 \
+      -e HYP_BASE_CHAINS_ANVIL2_CONNECTION_URL=http://127.0.0.1:8555 \
+      -e HYP_BASE_TRACING_LEVEL=info -e HYP_BASE_TRACING_FMT=pretty \
+      -e HYP_RELAYER_ORIGINCHAINNAME=$1 -e HYP_RELAYER_DESTINATIONCHAINNAMES=$2 \
+      -e HYP_RELAYER_ALLOWLOCALCHECKPOINTSYNCERS=true -e HYP_RELAYER_DB=/config/$1/relayer \
+      -e HYP_RELAYER_GASPAYMENTENFORCEMENT='[{"type":"none"}]' \
+      -e HYP_BASE_CHAINS_${3}_SIGNER_TYPE=hexKey \
+      -e HYP_BASE_CHAINS_${3}_SIGNER_KEY=0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97 \
+      gcr.io/abacus-labs-dev/hyperlane-agent:5bf8aed-20230323-140136 ./relayer &
+done
 
 
 
