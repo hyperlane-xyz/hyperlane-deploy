8fe5c9cb8d02827abbb02d518fa9d4e004bfb776
Asa Oines <asaoines@gmail.com>
2023-04-03 14:30:27 -0400

    ci

--- ci.sh
+++ ci.sh
@@ -8,6 +8,7 @@ sleep 1
 
 set -e
 
+
 DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil1 --remotes anvil2 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --no-write-agent-config
 DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil2 --remotes anvil1 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
