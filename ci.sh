9fa7228f90bc8229691ea494a4ff48b206413295
Asa Oines <asaoines@gmail.com>
2023-04-03 14:19:58 -0400

    ci

--- ci.sh
+++ ci.sh
@@ -1,10 +1,13 @@
-anvil --chain-id 31337 -p 8545 > /dev/null &
+anvil --chain-id 31337 -p 8545
 ANVIL_1_PID=$!
 
 anvil --chain-id 31338 -p 8555 > /dev/null &
 ANVIL_2_PID=$!
 
 sleep 5
+
+set -e
+
 DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil1 --remotes anvil2 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --no-write-agent-config
 DEBUG=hyperlane* yarn ts-node scripts/deploy.ts --local anvil2 --remotes anvil1 --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
