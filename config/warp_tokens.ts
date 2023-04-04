4465f522668406bb2df7d7e4fd5c4a25eb4da53c
J M Rossy <jm.rossy@gmail.com>
2023-04-04 17:27:55 +0900

    Implement WarpRouteDeployer Add hyperlane-token lib

--- config/warp_tokens.ts
+++ config/warp_tokens.ts
@@ -1,11 +1,11 @@
+import { HypERC20Config, TokenType } from '@hyperlane-xyz/hyperlane-token';
 import { ChainMap } from '@hyperlane-xyz/sdk';
-import { types } from '@hyperlane-xyz/utils';
 
-// Token configs for warp routes
+// A map of chain name to token configs
 // Not required for Hyperlane core deployments
-export const warpTokenConfig: ChainMap<WarpToken> = {
+export const warpTokenConfig: ChainMap<HypERC20Config> = {
   goerli: {
-    type: 'collateral',
+    type: TokenType.collateral,
     token: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
     // TODO get these from the deployment artifacts
     owner: '0x5bA371aeA18734Cb7195650aFdfCa4f9251aa513',
@@ -13,7 +13,7 @@ export const warpTokenConfig: ChainMap<WarpToken> = {
     interchainGasPaymaster: '0xF90cB82a76492614D07B82a7658917f3aC811Ac1',
   },
   alfajores: {
-    type: 'synthetic',
+    type: TokenType.synthetic,
     name: 'Weth',
     symbol: 'WETH',
     totalSupply: 0,
@@ -23,24 +23,3 @@ export const warpTokenConfig: ChainMap<WarpToken> = {
     interchainGasPaymaster: '0xF90cB82a76492614D07B82a7658917f3aC811Ac1',
   },
 };
-
-interface WarpTokenBase {
-  type: 'collateral' | 'synthetic';
-  owner: types.Address;
-  mailbox: types.Address;
-  interchainGasPaymaster: types.Address;
-}
-
-interface WarpCollateralToken extends WarpTokenBase {
-  type: 'collateral';
-  token: types.Address;
-}
-
-interface WarpSyntheticToken extends WarpTokenBase {
-  type: 'synthetic';
-  name: string;
-  symbol: string;
-  totalSupply: number;
-}
-
-type WarpToken = WarpCollateralToken | WarpSyntheticToken;
--- src/WarpRouteDeployer.ts
+++ src/WarpRouteDeployer.ts
@@ -1,8 +1,21 @@
-import { MultiProvider } from '@hyperlane-xyz/sdk';
+import {
+  ChainMap,
+  HyperlaneContracts,
+  MultiProvider,
+  serializeContractsMap,
+} from '@hyperlane-xyz/sdk';
 import yargs from 'yargs';
 
+import {
+  HypERC20Config,
+  HypERC20Deployer,
+  TokenType,
+} from '@hyperlane-xyz/hyperlane-token';
+import debug from 'debug';
 import { ethers } from 'ethers';
+import { warpTokenConfig } from '../config/warp_tokens';
 import { assertBalances, assertBytes32, getMultiProvider } from './config';
+import { mergeJSON } from './json';
 
 export async function getArgs(multiProvider: MultiProvider) {
   const args = await yargs(process.argv.slice(2))
@@ -11,10 +24,7 @@ export async function getArgs(multiProvider: MultiProvider) {
     .coerce('key', assertBytes32)
     .demandOption('key')
     .middleware(
-      assertBalances(
-        multiProvider,
-        () => ['goerli'], // TODO),
-      ),
+      assertBalances(multiProvider, () => Object.keys(warpTokenConfig)),
     );
   return args.argv;
 }
@@ -23,6 +33,7 @@ export class WarpRouteDeployer {
   constructor(
     public readonly multiProvider: MultiProvider,
     public readonly signer: ethers.Signer,
+    protected readonly logger = debug('hyperlane:WarpRouteDeployer'),
   ) {}
 
   static async fromArgs(): Promise<WarpRouteDeployer> {
@@ -34,6 +45,60 @@ export class WarpRouteDeployer {
   }
 
   async deploy(): Promise<void> {
-    // TODO
+    const tokenConfigs = this.getValidatedTokenConfig();
+    console.log('Starting deployments');
+    const deployer = new HypERC20Deployer(
+      // TODO update hyperlane-token to latest to fix type error
+      this.multiProvider,
+      tokenConfigs,
+      undefined,
+    );
+    await deployer.deploy();
+
+    const deployedContracts = deployer.deployedContracts as ChainMap<
+      HyperlaneContracts<any>
+    >;
+    const tokenAddrs = serializeContractsMap(deployedContracts);
+    // Write contract address artifacts
+    mergeJSON('./artifacts/', 'warp-tokens.json', tokenAddrs);
+  }
+
+  getValidatedTokenConfig(): ChainMap<HypERC20Config> {
+    const targetChains = Object.keys(warpTokenConfig);
+    if (!targetChains.length)
+      throw new Error('No chains found in warp token config');
+    // Ensure MP has config for each chain
+    targetChains.forEach((chain) => this.multiProvider.getChainMetadata(chain));
+
+    const collateralOrNativeTokens = Object.values(warpTokenConfig).filter(
+      (t) => [TokenType.collateral, TokenType.native].includes(t.type),
+    );
+    if (collateralOrNativeTokens.length !== 1)
+      throw new Error(
+        'Exactly one collateral or native token must be configured',
+      );
+
+    const syntheticTokens = Object.values(warpTokenConfig).filter(
+      (t) => t.type === TokenType.synthetic,
+    );
+    if (syntheticTokens.length < 1)
+      throw new Error('At least one synthetic token must be configured');
+
+    const uriTokens = Object.values(warpTokenConfig).filter((t) =>
+      [TokenType.collateralUri, TokenType.syntheticUri].includes(t.type),
+    );
+    if (uriTokens.length > 0)
+      throw new Error(
+        'No uri tokens should be configured for an ERC20 warp route',
+      );
+
+    // TODO validate token metadata against chain
+
+    this.logger(
+      `Found token configs for ${targetChains.length} chains:`,
+      targetChains.join(', '),
+    );
+
+    return warpTokenConfig;
   }
 }
