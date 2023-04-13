e7338780f80f06012a6e4a04f9fb5b54d5d04d9b
J M Rossy <jm.rossy@gmail.com>
2023-04-13 21:35:32 +0900

    Revert ci.sh changes

--- ci.sh
+++ ci.sh
@@ -29,11 +29,6 @@ echo "Deploying warp routes"
 yarn ts-node scripts/deploy-warp-routes.ts \
   --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
-echo "===Token list json==="
-cat ./artifacts/warp-ui-token-list.json
-echo "===Token list TS==="
-cat ./artifacts/warp-ui-token-list.ts
-
 kill $ANVIL_1_PID
 kill $ANVIL_2_PID
 
