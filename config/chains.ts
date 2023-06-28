84e69db0f16e683ee9b86912144fc30aa516d021
Nam Chu Hoai <nambrot@googlemail.com>
2023-06-28 11:55:28 -0400

    Indicate the ability to override defaults

--- config/chains.ts
+++ config/chains.ts
@@ -19,6 +19,10 @@ export const chains: ChainMap<ChainMetadata> = {
         http: 'http://127.0.0.1:8545',
       },
     ],
+    // You can set overrides for transaction fields here
+    // transactionOverrides: {
+    //   gasLimit: 1000000
+    // },
   },
   anvil2: {
     name: 'anvil2',
@@ -29,4 +33,13 @@ export const chains: ChainMap<ChainMetadata> = {
       },
     ],
   },
+  // --------------------------------------------------
+  // You can also override the default chain metadata
+  // ethereum: {
+  //   publicRpcUrls: [
+  //     {
+  //       http: 'my.custom.rpc.url',
+  //     }
+  //   ],
+  // }
 };
