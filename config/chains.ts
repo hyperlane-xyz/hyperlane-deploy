3ddc09c4d6d92549a07fc0148b81a844f06c61fb
Nam Chu Hoai <nambrot@googlemail.com>
2023-07-01 11:03:01 -0400

    Indicate the ability to override defaults (#60)

--- config/chains.ts
+++ config/chains.ts
@@ -1,5 +1,6 @@
 import { ChainMap, ChainMetadata } from '@hyperlane-xyz/sdk';
 
+// import { chainMetadata } from '@hyperlane-xyz/sdk';
 // A map of chain names to ChainMetadata
 export const chains: ChainMap<ChainMetadata> = {
   // ----------- Add your chains here -----------------
@@ -19,6 +20,10 @@ export const chains: ChainMap<ChainMetadata> = {
         http: 'http://127.0.0.1:8545',
       },
     ],
+    // You can set overrides for transaction fields here
+    // transactionOverrides: {
+    //   gasLimit: 1000000
+    // },
   },
   anvil2: {
     name: 'anvil2',
@@ -29,4 +34,14 @@ export const chains: ChainMap<ChainMetadata> = {
       },
     ],
   },
+  // --------------------------------------------------
+  // You can also override the default chain metadata (completely)
+  // ethereum: {
+  //   ...chainMetadata.ethereum,
+  //   publicRpcUrls: [
+  //     {
+  //       http: 'my.custom.rpc.url',
+  //     }
+  //   ],
+  // }
 };
