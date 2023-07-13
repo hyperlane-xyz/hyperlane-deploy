1dfb2ddb10499e869728dc6fbd030c1fea9175c2
steve806 <steve8060914@gmail.com>
2023-07-14 01:36:26 +0900

    add: mitosis config

--- config/chains.ts
+++ config/chains.ts
@@ -4,27 +4,30 @@ import { ChainMap, ChainMetadata, ProtocolType } from '@hyperlane-xyz/sdk';
 // A map of chain names to ChainMetadata
 export const chains: ChainMap<ChainMetadata> = {
   // ----------- Add your chains here -----------------
+  mitosis: {
+    name: 'mitosis',
+    protocol: ProtocolType.Ethereum,
+    chainId: 26657,
+    rpcUrls: [
+      {
+        http: 'http://127.0.0.1:8545',
+      },
+    ],
+  },
   anvil1: {
     name: 'anvil1',
     protocol: ProtocolType.Ethereum,
-    // anvil default chain id
     chainId: 31338,
-    // Used to configure a Warp Route to bridge anvil1 ETH
-    // to anvil2 in CI tests.
-    nativeToken: {
-      name: 'ether',
-      symbol: 'ETH',
-      decimals: 18,
-    },
+    // nativeToken: {
+    //   name: 'ether',
+    //   symbol: 'ETH',
+    //   decimals: 18,
+    // },
     rpcUrls: [
       {
         http: 'http://127.0.0.1:8546',
       },
     ],
-    // You can set overrides for transaction fields here
-    // transactionOverrides: {
-    //   gasLimit: 1000000
-    // },
   },
   anvil2: {
     name: 'anvil2',
@@ -36,14 +39,4 @@ export const chains: ChainMap<ChainMetadata> = {
       },
     ],
   },
-  // --------------------------------------------------
-  // You can also override the default chain metadata (completely)
-  // ethereum: {
-  //   ...chainMetadata.ethereum,
-  //   publicRpcUrls: [
-  //     {
-  //       http: 'my.custom.rpc.url',
-  //     }
-  //   ],
-  // }
 };
--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -2,6 +2,11 @@ import { ChainMap, ModuleType, MultisigIsmConfig } from '@hyperlane-xyz/sdk';
 
 export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
   // ----------- Your chains here -----------------
+  mitosis: {
+    type: ModuleType.LEGACY_MULTISIG,
+    threshold: 1,
+    validators: ['0x26bd6c9FE7765A796F33A190a480272ea14eeab0'],
+  },
   anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
