b9f3aea184127bf15703dd7dd6d7c4f1f1909643
steve806 <steve8060914@gmail.com>
2023-07-14 01:52:18 +0900

    fix: mito prefix cosmos address?

--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -7,7 +7,8 @@ export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
     threshold: 1,
     validators: [
       // '0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'
-      '0x4B0a1a38A14BbB74AE6a63d7eff65470557a498d',
+      // '0x4B0a1a38A14BbB74AE6a63d7eff65470557a498d',
+      'mito1t9xschet4psjznr5x980fvyj0f0x7r9em3q7e4',
     ],
   },
   anvil1: {
