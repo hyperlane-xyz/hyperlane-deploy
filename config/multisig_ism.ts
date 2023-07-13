4ffbf7ce894e5eeceeb6e0733027e0b7f9a9a5dd
steve806 <steve8060914@gmail.com>
2023-07-14 01:46:42 +0900

    fix: mitosis validator address

--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -5,7 +5,7 @@ export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
   mitosis: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: ['0x26bd6c9FE7765A796F33A190a480272ea14eeab0'],
+    validators: ['0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'],
   },
   anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
