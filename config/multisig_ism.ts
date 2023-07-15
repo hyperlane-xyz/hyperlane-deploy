ba5e741a0a223e341d6b862e201c7947daf8b39b
steve806 <steve8060914@gmail.com>
2023-07-15 18:08:21 +0900

    fix: mitosis validators address

--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -5,18 +5,14 @@ export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
   mitosis: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
-    validators: [
-      // '0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'
-      // '0x4B0a1a38A14BbB74AE6a63d7eff65470557a498d',
-      'mito1t9xschet4psjznr5x980fvyj0f0x7r9em3q7e4',
-    ],
+    validators: ['0x594D0C5F2ba861214C74314eF4b0927A5E6f0Cb9'],
   },
   anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
     threshold: 1,
     validators: [
       // Last anvil address
-      '0x26bd6c9FE7765A796F33A190a480272ea14eeab0',
+      '0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a',
     ],
   },
   anvil2: {
@@ -24,7 +20,7 @@ export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
     threshold: 1,
     validators: [
       // Last anvil address
-      '0x26bd6c9FE7765A796F33A190a480272ea14eeab0',
+      '0x3a4c9ad12b4daF20ae4635F9c2C0a03af583558a',
     ],
   },
 };
