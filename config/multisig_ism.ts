7704a360eb17b1e2d6f4e4a37b6666a04ad4e29b
Trevor Porter <trkporter@ucdavis.edu>
2023-08-16 11:51:57 +0100

    Get rid of deploy-specific configs

--- config/multisig_ism.ts
+++ config/multisig_ism.ts
@@ -2,22 +2,20 @@ import { ChainMap, ModuleType, MultisigIsmConfig } from '@hyperlane-xyz/sdk';
 
 export const multisigIsmConfig: ChainMap<MultisigIsmConfig> = {
   // ----------- Your chains here -----------------
-  solana: {
+  anvil1: {
     type: ModuleType.LEGACY_MULTISIG,
-    threshold: 2,
+    threshold: 1,
     validators: [
-      '0x3cd1a081f38874bbb075bf10b62adcb858db864c',
-      '0x28aa072634dd41d19471640237852e807bd9901f',
-      '0x8a93ba04f4e30064660670cb581d9aa10df78929',
+      // Last anvil address
+      '0xa0ee7a142d267c1f36714e4a8f75612f20a79720',
     ],
   },
-  nautilus: {
+  anvil2: {
     type: ModuleType.LEGACY_MULTISIG,
-    threshold: 2,
+    threshold: 1,
     validators: [
-      '0x9c920af9467595a23cb3433adefc3854d498a437',
-      '0x12b583ce1623b7de3fc727ccccda24dcab1fe022',
-      '0xc8b996a421ff1e203070c709c1af93944c049cc0',
+      // Last anvil address
+      '0xa0ee7a142d267c1f36714e4a8f75612f20a79720',
     ],
   },
 };
--- config/warp_tokens.ts
+++ config/warp_tokens.ts
@@ -7,10 +7,10 @@ import type { WarpRouteConfig } from '../src/warp/config';
 export const warpRouteConfig: WarpRouteConfig = {
   base: {
     // Chain name must be in the Hyperlane SDK or in the chains.ts config
-    chainName: 'bsc',
-    type: TokenType.collateral, //  TokenType.native or TokenType.collateral
+    chainName: 'anvil1',
+    type: TokenType.native, //  TokenType.native or TokenType.collateral
     // If type is collateral, a token address is required:
-    address: '0x37a56cdcD83Dce2868f721De58cB3830C44C6303',
+    // address: '0x123...'
     // If the token is an NFT (ERC721), set to true:
     // isNft: boolean
 
@@ -19,25 +19,13 @@ export const warpRouteConfig: WarpRouteConfig = {
   },
   synthetics: [
     {
-      chainName: 'nautilus',
-      type: TokenType.native,
-      scale: 10 ** 9,
+      chainName: 'anvil2',
 
       // Optionally specify a name, symbol, and totalSupply
       // If not specified, the base token's properties will be used
 
       // Optionally, specify owner, mailbox, and interchainGasPaymaster addresses
       // If not specified, the Permissionless Deployment artifacts or the SDK's defaults will be used
-    },
-    {
-      chainName: 'solana',
-      type: TokenType.collateral,
-      address: '0x0000000000000000000000000000000000000000',
-      name: 'Zebec',
-      symbol: 'ZBC',
-      decimals: 9,
-      foreignDeployment:
-        '0xc5ba229fa2822fe65ac2bd0a93d8371d75292c3415dd381923c1088a3308528b',
     },
   ],
 };
